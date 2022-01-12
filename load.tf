1973299230f95d2473a2fc18608e804cd0e31370
nicholasutikal <nicholasutikal@gmail.com>
2022-01-12 14:51:34 +0100

    adds cloud build file var

--- load.tf
+++ load.tf
@@ -4,5 +4,6 @@ module "load" {
   project                                       = var.project
   location                                      = var.location
   branch                                        = var.branch
-  env_suffix                                    = var.project
+  env_suffix                                    = var.env_suffix
+  cloudbuild_file                               = var.cloudbuild_file
 }
