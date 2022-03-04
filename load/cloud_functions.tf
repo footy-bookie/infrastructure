10a1b58df19cc363fa780fc74dd215e7c562d542
nicholasutikal <nicholasutikal@gmail.com>
2022-03-04 17:50:01 +0100

    readds all ressources

--- load/cloud_functions.tf
+++ load/cloud_functions.tf
@@ -1,55 +1,55 @@
-############################
-## VM START CLOUD FUNCTION #
-############################
-#resource "google_cloudfunctions_function" "StartServer" {
-#  name                  = "StartServer"
-#  description           = "Starts footy import vm"
-#  runtime               = "python39"
-#  project               = var.project
-#  available_memory_mb   = 1024
-#  timeout               = 30
-#  trigger_http          = true
-#  max_instances         = 5
-#  entry_point           = "main"
-#  region                = var.location
-#  # invocation service account or ingress_settings = "ALLOW_INTERNAL_ONLY" setting,not both
-#  service_account_email = "fake-client-project@appspot.gserviceaccount.com"
-#  # make sure that paths (currently src) is the same in here and in the corresponding cloud build trigger
-#  source_repository {
-#    url = "https://source.developers.google.com/projects/${var.project}/repos/${var.start_import_vm_repo}/moveable-aliases/${var.branch}/paths/src"
-#  }
-#
-#  environment_variables = {
-#    PROJECT_NUMBER = var.project_number
-#    INSTANCE_NAME  = var.instance_name
-#    INSTANCE_ZONE  = var.zone
-#  }
-#}
-#
-##########################
-## VM END CLOUD FUNCTION #
-##########################
-#resource "google_cloudfunctions_function" "EndServer" {
-#  name                  = "EndServer"
-#  description           = "End footy import vm"
-#  runtime               = "python39"
-#  project               = var.project
-#  available_memory_mb   = 1024
-#  timeout               = 30
-#  trigger_http          = true
-#  max_instances         = 5
-#  entry_point           = "main"
-#  region                = var.location
-#  # invocation service account or ingress_settings = "ALLOW_INTERNAL_ONLY" setting,not both
-#  service_account_email = "fake-client-project@appspot.gserviceaccount.com"
-#  # make sure that paths (currently src) is the same in here and in the corresponding cloud build trigger
-#  source_repository {
-#    url = "https://source.developers.google.com/projects/${var.project}/repos/${var.end_import_vm_repo}/moveable-aliases/${var.branch}/paths/src"
-#  }
-#
-#  environment_variables = {
-#    PROJECT_NUMBER = var.project_number
-#    INSTANCE_NAME  = var.instance_name
-#    INSTANCE_ZONE  = var.zone
-#  }
-#}
+###########################
+# VM START CLOUD FUNCTION #
+###########################
+resource "google_cloudfunctions_function" "StartServer" {
+  name                  = "StartServer"
+  description           = "Starts footy import vm"
+  runtime               = "python39"
+  project               = var.project
+  available_memory_mb   = 1024
+  timeout               = 30
+  trigger_http          = true
+  max_instances         = 5
+  entry_point           = "main"
+  region                = var.location
+  # invocation service account or ingress_settings = "ALLOW_INTERNAL_ONLY" setting,not both
+  service_account_email = "footy-dev-343115@appspot.gserviceaccount.com"
+  # make sure that paths (currently src) is the same in here and in the corresponding cloud build trigger
+  source_repository {
+    url = "https://source.developers.google.com/projects/${var.project}/repos/${var.start_import_vm_repo}/moveable-aliases/${var.branch}/paths/src"
+  }
+
+  environment_variables = {
+    PROJECT_NUMBER = var.project_number
+    INSTANCE_NAME  = var.instance_name
+    INSTANCE_ZONE  = var.zone
+  }
+}
+
+#########################
+# VM END CLOUD FUNCTION #
+#########################
+resource "google_cloudfunctions_function" "EndServer" {
+  name                  = "EndServer"
+  description           = "End footy import vm"
+  runtime               = "python39"
+  project               = var.project
+  available_memory_mb   = 1024
+  timeout               = 30
+  trigger_http          = true
+  max_instances         = 5
+  entry_point           = "main"
+  region                = var.location
+  # invocation service account or ingress_settings = "ALLOW_INTERNAL_ONLY" setting,not both
+  service_account_email = "footy-dev-343115@appspot.gserviceaccount.com"
+  # make sure that paths (currently src) is the same in here and in the corresponding cloud build trigger
+  source_repository {
+    url = "https://source.developers.google.com/projects/${var.project}/repos/${var.end_import_vm_repo}/moveable-aliases/${var.branch}/paths/src"
+  }
+
+  environment_variables = {
+    PROJECT_NUMBER = var.project_number
+    INSTANCE_NAME  = var.instance_name
+    INSTANCE_ZONE  = var.zone
+  }
+}
--- load/scheduler.tf
+++ load/scheduler.tf
@@ -1,49 +1,49 @@
-#resource "google_cloud_scheduler_job" "start_vm_scheduler" {
-#  name             = "static_data_start_import_scheduler"
-#  description      = "Triggers the start vm Cloud Function to start vm"
-#  schedule         = "45 17 * * 4"
-#  time_zone        = "Europe/Berlin"
-#  region           = var.location
-#  attempt_deadline = "540s"
-#  project          = var.project
-#
-#  retry_config {
-#    retry_count = 0
-#  }
-#
-#  http_target {
-#    http_method = "POST"
-#    uri         = "https://${var.location}-${var.project}.cloudfunctions.net/${google_cloudfunctions_function.StartServer.name}"
-#
-#    # see here how body must look like
-#    # https://www.reddit.com/r/googlecloud/comments/lrv9eo/cant_pass_any_arguments_to_a_workflow_when_using/
-#    oidc_token {
-#      service_account_email = google_service_account.starts_stats_import_sa.email
-#    }
-#  }
-#}
-#
-#  resource "google_cloud_scheduler_job" "end_vm_scheduler" {
-#  name             = "static_data_end_import_scheduler"
-#  description      = "Triggers the end vm Cloud Function to start vm"
-#  schedule         = "0 21 * * MON-FRI"
-#  time_zone        = "Europe/Berlin"
-#  region           = var.location
-#  attempt_deadline = "540s"
-#  project          = var.project
-#
-#  retry_config {
-#    retry_count = 0
-#  }
-#
-#  http_target {
-#    http_method = "POST"
-#    uri         = "https://${var.location}-${var.project}.cloudfunctions.net/${google_cloudfunctions_function.EndServer.name}"
-#
-#    # see here how body must look like
-#    # https://www.reddit.com/r/googlecloud/comments/lrv9eo/cant_pass_any_arguments_to_a_workflow_when_using/
-#    oidc_token {
-#      service_account_email = google_service_account.ends_stats_import_sa.email
-#    }
-#  }
-#}
+resource "google_cloud_scheduler_job" "start_vm_scheduler" {
+  name             = "static_data_start_import_scheduler"
+  description      = "Triggers the start vm Cloud Function to start vm"
+  schedule         = "45 17 * * 4"
+  time_zone        = "Europe/Berlin"
+  region           = var.location
+  attempt_deadline = "540s"
+  project          = var.project
+
+  retry_config {
+    retry_count = 0
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${var.location}-${var.project}.cloudfunctions.net/${google_cloudfunctions_function.StartServer.name}"
+
+    # see here how body must look like
+    # https://www.reddit.com/r/googlecloud/comments/lrv9eo/cant_pass_any_arguments_to_a_workflow_when_using/
+    oidc_token {
+      service_account_email = google_service_account.starts_stats_import_sa.email
+    }
+  }
+}
+
+  resource "google_cloud_scheduler_job" "end_vm_scheduler" {
+  name             = "static_data_end_import_scheduler"
+  description      = "Triggers the end vm Cloud Function to start vm"
+  schedule         = "0 21 * * MON-FRI"
+  time_zone        = "Europe/Berlin"
+  region           = var.location
+  attempt_deadline = "540s"
+  project          = var.project
+
+  retry_config {
+    retry_count = 0
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${var.location}-${var.project}.cloudfunctions.net/${google_cloudfunctions_function.EndServer.name}"
+
+    # see here how body must look like
+    # https://www.reddit.com/r/googlecloud/comments/lrv9eo/cant_pass_any_arguments_to_a_workflow_when_using/
+    oidc_token {
+      service_account_email = google_service_account.ends_stats_import_sa.email
+    }
+  }
+}
