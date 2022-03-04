240ca21238f26c4fbbc62c75c2d89b7be068f007
nicholasutikal <nicholasutikal@gmail.com>
2022-03-04 17:34:50 +0100

    test

--- load/storage.tf
+++ load/storage.tf
@@ -1,5 +1,5 @@
-#resource "google_storage_bucket" "footy_stats_sink" {
-#  name     = "${var.env_suffix}-footy_stats_sink_dev"
-#  location = var.location
-#  project  = var.project
-#}
+resource "google_storage_bucket" "footy_stats_sink" {
+  name     = "${var.env_suffix}-footy_stats_sink_dev"
+  location = var.location
+  project  = var.project
+}
