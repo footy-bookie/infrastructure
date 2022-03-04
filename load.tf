b55e21c5ac085c14c8885f90023422af2d1ca48f
nicholasutikal <nicholasutikal@gmail.com>
2022-03-04 17:02:13 +0100

    asdasd

--- load.tf
+++ load.tf
@@ -1,16 +1,16 @@
-module "load" {
-  source = "./load/"
-
-  project              = var.project
-  location             = var.location
-  zone                 = var.zone
-  branch               = var.branch
-  env_suffix           = var.env_suffix
-  cloudbuild_file      = var.cloudbuild_file
-  project_number       = var.project_number
-  footy_username       = var.footy_username
-  footy_key_name       = var.footy_key_name
-  instance_name        = var.instance_name
-  start_import_vm_repo = var.start_import_vm_repo
-  end_import_vm_repo  = var.end_import_vm_repo
-}
+#module "load" {
+#  source = "./load/"
+#
+#  project              = var.project
+#  location             = var.location
+#  zone                 = var.zone
+#  branch               = var.branch
+#  env_suffix           = var.env_suffix
+#  cloudbuild_file      = var.cloudbuild_file
+#  project_number       = var.project_number
+#  footy_username       = var.footy_username
+#  footy_key_name       = var.footy_key_name
+#  instance_name        = var.instance_name
+#  start_import_vm_repo = var.start_import_vm_repo
+#  end_import_vm_repo  = var.end_import_vm_repo
+#}
